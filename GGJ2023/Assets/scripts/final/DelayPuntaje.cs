893d7282f337bc5036de330d2d134f32a5db40d8
Ricardo <60669370+Bleysiker@users.noreply.github.com>
2023-02-04 17:13:34 -0500

    nuevo mensaje final

--- GGJ2023/Assets/scripts/final/DelayPuntaje.cs
+++ GGJ2023/Assets/scripts/final/DelayPuntaje.cs
@@ -5,23 +5,25 @@ using TMPro;
 
 public class DelayPuntaje : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI puntaje;
-    [SerializeField] CanvasGroup transparencia;
+    [SerializeField] TextMeshProUGUI puntaje,opcion;
+    [SerializeField] CanvasGroup transparencia,tpO;
     [SerializeField] REvents reproducirAnim;
     [SerializeField] float aparicion,tiempoAnim,tiempoPantalla;
     [SerializeField] int puntajeFinal;
-    [SerializeField] string mensajeFinal;
+    [SerializeField] string mensajeFinal,mensajeOpcion;
     void Start()
     {
+        opcion.text = mensajeOpcion;
         if (PlayerPrefs.GetInt("puntaje") < puntajeFinal)
         {
-            puntaje.text = "Te faltaron " + (puntajeFinal-PlayerPrefs.GetInt("puntaje")) + " m"+ " para alcanzar el cielo";
+            puntaje.text = "Te faltaron " + (puntajeFinal - PlayerPrefs.GetInt("puntaje")) + " m" + " para alcanzar el cielo";
         }
         else
         {
             puntaje.text = mensajeFinal;
         }
         transparencia.alpha = 0;
+        tpO.alpha = 0;
         StartCoroutine(MostrarPuntaje());
     }
 
@@ -31,5 +33,7 @@ public class DelayPuntaje : MonoBehaviour
         reproducirAnim.FireEvent();
         yield return new WaitForSeconds(tiempoAnim);
         transparencia.LeanAlpha(1, aparicion);
+        yield return new WaitForSeconds(1f);
+        tpO.LeanAlpha(1, aparicion);
     }
 }
